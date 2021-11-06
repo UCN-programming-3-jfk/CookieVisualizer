0549bb239ac61accd0e6468aed39db43b1722d2f
Jakob Farian Krarup <kr@rup.dk>
2021-11-06 09:53:50 +0100

    Add project files.

---
+++ Cookies/Models/Cookie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Cookies.Models
+{
+    public class Cookie
+    {
+        [Required]
+        public string CookieName { get; set; }
+        [Required]
+        public string CookieValue { get; set; }
+
+    }
+}
